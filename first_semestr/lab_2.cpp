cee01697cf17cfffca9d14b6c00aa1840d43aff5
BorisTitarenko <specmohnat2309@gmail.com>
2018-06-02 21:47:39 +0300

    deleted Cyrillic comments

--- first_semestr/lab_2.cpp
+++ first_semestr/lab_2.cpp
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <math.h>//бібліотека, необхідна для математичних розрахунків
+#include <math.h>
 int main()
 {
 	float x, y, z, result;
-	scanf_s("%f", &x, "\n");// зчитую змінні по порядку
+	scanf_s("%f", &x, "\n");
 	scanf_s("%f", &y, "\n");
 	scanf_s("%f", &z, "\n");
-	result = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));//ось і сам вираз
-	printf("%f", result);//вивожу на екран
+	result = sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+	printf("%f", result);
 	getchar();
 	getchar();
 }
