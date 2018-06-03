aae3a9db212510b5d712aaa8134b885a9c5b8db9
BorisTitarenko <specmohnat2309@gmail.com>
2018-06-03 19:03:10 +0300

    added nummerical methods

---
+++ second_semestr/Numerical methods/1/header.h
@@ -0,0 +1,14 @@
+//
+// Created by Borys on 13.02.2018.
+//
+
+#ifndef LAB1_HEADER_H
+#define LAB1_HEADER_H
+
+double dihom(double a, double b, double e);
+double hord(double a, double b, double e);
+int input(double & a, double  & b, double & i);
+double f(double x);
+double ddf(double x);
+void  print(int i, double x, double a);
+#endif //LAB1_HEADER_H
